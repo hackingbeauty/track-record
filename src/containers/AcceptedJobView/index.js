2eca33e59f522d015f0169998ab3d39353058f0f
Mark Muskardin <mark.muskardin@gmail.com>
2018-11-10 19:48:58 -0800

    Added AcceptedJobView

---
+++ src/containers/AcceptedJobView/index.js
@@ -0,0 +1,94 @@
+import React, { Component }   from 'react'
+import PropTypes              from 'prop-types'
+import { connect }            from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter }         from 'react-router'
+import * as jobActionCreators from 'core/actions/actions-job'
+import * as uiActionCreators  from 'core/actions/actions-ui'
+import AppBar                 from 'components/AppBar'
+import { StandardModal }      from 'components/Modals'
+import Slide                  from '@material-ui/core/Slide'
+import Toolbar                from '@material-ui/core/Toolbar'
+import IconButton             from '@material-ui/core/IconButton'
+import ArrowBackIcon          from '@material-ui/icons/ArrowBack'
+import Typography             from 'components/Typography'
+import { styles }             from './styles.scss'
+
+function TransitionComponent(props) {
+  return <Slide direction="left" {...props} mountOnEnter unmountOnExit />
+}
+
+class AcceptedJobView extends Component {
+  componentDidMount() {
+    const { actions } = this.props
+
+    actions.ui.openModal({
+      modalKey: 'accepted-job-modal'
+    })
+  }
+
+  close= () => {
+    const { history } = this.props
+    history.push('/job')
+  }
+
+  render() {
+    const { ui } = this.props
+
+    return (
+      <StandardModal
+        modalKey="accepted-job-modal"
+        modalState={ui.modalState}
+        onClose={this.close}
+        TransitionComponent={TransitionComponent}
+      >
+        <div>
+          <AppBar>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="back-arrow"
+                onClick={this.close}
+                className="arrow-icon"
+              >
+                <ArrowBackIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit">
+                Update Job Status
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <div className={styles}>
+            <div className="container">
+              accepted job view here
+            </div>
+          </div>
+        </div>
+      </StandardModal>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    ui: state.ui
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      job: bindActionCreators(jobActionCreators, dispatch),
+      ui: bindActionCreators(uiActionCreators, dispatch)
+    }
+  }
+}
+
+AcceptedJobView.propTypes = {
+  actions: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({}).isRequired,
+  ui: PropTypes.shape({}).isRequired
+}
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AcceptedJobView))
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,13 +6,14 @@ import {
   Redirect,
   Switch
 } from 'react-router-dom'
-import theme      from 'configs/theme/config-theme'
-import HomeView   from 'containers/HomeView'
-import StartView  from 'containers/StartView'
-import JobView    from 'containers/JobView'
-import StatusView from 'containers/StatusView'
-import Header     from './components/Header'
-import Footer     from './components/Footer'
+import theme           from 'configs/theme/config-theme'
+import HomeView        from 'containers/HomeView'
+import StartView       from 'containers/StartView'
+import JobView         from 'containers/JobView'
+import AcceptedJobView from 'containers/AcceptedJobView'
+import StatusView      from 'containers/StatusView'
+import Header          from './components/Header'
+import Footer          from './components/Footer'
 
 import './styles.scss' // global styles
 
@@ -29,6 +30,7 @@ class App extends Component {
                 <Route path="/start" component={StartView} />
                 <Route path="/home" component={HomeView} />
                 <Route path="/job" component={JobView} />
+                <Route path="/accepted" component={AcceptedJobView} />
                 <Route path="/status" component={StatusView} />
                 <Redirect from="/" to="/start" />
               </Switch>
