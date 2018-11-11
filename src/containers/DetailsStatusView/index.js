6f4a45efae0de1d54966c1439319175230343761
Mark Muskardin <mark.muskardin@gmail.com>
2018-11-11 11:50:06 -0800

    fixes

---
+++ src/containers/DetailsStatusView/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { styles }           from './styles.scss'
+
+class DetailsStatusView extends Component {
+  render() {
+    return (
+      <div className={styles}>detailsstatus view</div>
+    )
+  }
+}
+
+export default DetailsStatusView
